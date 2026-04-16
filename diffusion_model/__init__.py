cd23a7924999697b81bac04edc4af1eeb33a078c
Adama Camara <adamabusiness24@gmail.com>
2026-04-15 21:14:53 -0300

    update train.py and utils.py

--- diffusion_model/__init__.py
+++ diffusion_model/__init__.py
@@ -3,7 +3,7 @@ from .model import LegacyUNet, UNet
 from .scheduler import linear_beta_schedule
 from .noise import q_sample, p_sample_loop, sample_timesteps
 from .trainer import Trainer
-from .utils import get_dataloader
+from .utils import get_dataloader, load_model_from_checkpoint
 
 __all__ = [
     "LegacyUNet",
